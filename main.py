75537e7b9311ecf36fd42d6e9657b41cfb82c4d6
Ella Moss <ella.moss@utah.edu>
2021-07-20 17:25:15 -0600

    let's learn android app development

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ import time
 import sys
 import os
 
-tracks = importPlaylist(filename)
-songs = getMetadataFromDevice(tracks)
+def readPlaylist(filename):
+    songList=[]
+    with open(filename,"r") as f:
+        songList=f.read().splitlines()
+    return songList
+
+def getMetadataFromDevice(paths):
+    for path in paths:
+        #Idk if I can do this from adb.
+        #Making a helper app I guess. 
+        #Who knows, maybe in the future I can port this to all-Android.
+        metadata = subprocess.Popen('adb shell run com.ella.helperapp MainActivity');
+
+filename='playlist.m3u8'
+
+paths = readPlaylist(filename)
+songs = getMetadataFromDevice(paths)
 createPlaylist(name)
 videos = searchForVideos(songs)
 addVideos(videos,name)
