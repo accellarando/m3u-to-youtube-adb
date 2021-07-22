177a85d655687984f473c420c06ca2b0f023886c
Ella Moss <ella.moss@utah.edu>
2021-07-22 14:20:55 -0600

    readme changes, gitignore

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 My music library is just on my phone, I make playlists there, I store songs there.
 
 This makes sharing playlists kinda hard. So this is my stab at translating these m3u playlists into a YouTube playlist.
+
+## Setting up your YouTube API credentials.
+
+Refer to [this reference](https://developers.google.com/youtube/v3/quickstart/python) from Google.
+1. Create a project in the [API console](https://console.cloud.google.com/projectselector2/apis/dashboard).
+	- Agree to the ToS and click "Create Project." Give it a name.
+2. From Library, enable the YouTube Data API v3.A
+3. Create your credentials. Navigate to Credentials, and create an API key and OAuth client ID.
+	- For the OAuth key, set the application type to "Other".
+4. Copy your credentials into config.py.
+
+## Dependencies
+- Android Debug Bridge
+- Python modules (install with Pip):
+	- eyeD3
+	- google-api-python-client
+	- google-auth-oauthlib
+	- google-auth-httplib2
--- main.py
+++ main.py
@@ -1,27 +1,78 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.9
 
 import subprocess
 import time
 import sys
 import os
+from shlex import quote 
+import eyed3
 
 def readPlaylist(filename):
-    songList=[]
+    songList = []
     with open(filename,"r") as f:
-        songList=f.read().splitlines()
+        for line in f.readlines():
+            if not line.startswith("#"):
+                songList.append("/storage/"+line.rstrip())
     return songList
 
 def getMetadataFromDevice(paths):
+    global badPaths
+    metadata = []
     for path in paths:
-        #Idk if I can do this from adb.
-        #Making a helper app I guess. 
-        #Who knows, maybe in the future I can port this to all-Android.
-        metadata = subprocess.Popen('adb shell run com.ella.helperapp MainActivity');
+        tags = {}
+        escapedPath = quote(path)
+        if(escapedPath[0]=="'"):
+            escapedPath='"'+escapedPath[1:-1]+'"'
 
-filename='playlist.m3u8'
+        footerCommand = "adb shell 'tail -c 128 "+escapedPath+" |cat -v'"
+        footer = subprocess.check_output(footerCommand,shell=True)
+        if footer.decode()[0:3] == "TAG":
+            #This should be faster, try this first
+            tags = parseV1(footer)
+        else:
+            #pull is different than shell for escaping, ig
+            # path = path.replace("'\"'\"'","\\'")
+            headerCommand = "adb pull \""+path+"\" tmp.mp3; head -c 3 tmp.mp3"
+            headerTag = subprocess.check_output(headerCommand,shell=True)
+            if(headerTag.decode()[-3:] == "ID3"):
+                tags = parseV2('tmp.mp3')
+                subprocess.run(['rm','tmp.mp3'])
+            else:
+                badPaths.append(path)
+        if len(tags) > 0:
+            metadata.append(tags)
+    return badPaths,metadata
+
+def parseV1(adbOutput):
+    '''Parses info from a ID3v1 tag.'''
+    rawMetadata = adbOutput.decode().replace("^@"," ")
+    title=rawMetadata[3:32]
+    artist=rawMetadata[33:62]
+    album=rawMetadata[63:92]
+    year=rawMetadata[93:97]
+    return {
+        "title": title.strip(),
+        "artist": artist.strip(),
+        "album": album.strip()
+    }
+
+
+def parseV2(path):
+    song = eyed3.load(path)
+    return {
+        "title": song.tag.title,
+        "artist": song.tag.artist,
+        "album": song.tag.album
+    }
+
+
+filename = 'playlist.m3u8'
+badPaths = []
 
 paths = readPlaylist(filename)
 songs = getMetadataFromDevice(paths)
+print(songs)
+youtubeLogin(
 createPlaylist(name)
 videos = searchForVideos(songs)
 addVideos(videos,name)
